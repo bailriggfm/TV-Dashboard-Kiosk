82dd67b44a06a7dc2c16b9a8867f20584654c3ff
Ava Glass <AvaAGlass@outlook.com>
2025-05-20 18:20:29 +0100

    Dont use static password. Generate a new one so it has a unique has at the min

--- build.sh
+++ build.sh
@@ -9,9 +9,15 @@ fi
 
 # CONFIG
 ISO_LABEL="ArchLinux-Kiosk"
-CONFIG_DIR="./"
 ISO1_EXEC='ExecStart=/usr/bin/cage -s -- /usr/bin/firefox --kiosk --no-remote --private --start-fullscreen --disable-popup-blocking --no-proxy-server --private-window "https://google.com"'
+USERPASSWORD="liveuser"
+
+# Do not change these unless you have changed the coressponding files.
+CONFIG_DIR="./"
 TARGET_FILE="${CONFIG_DIR}/airootfs/etc/systemd/system/cage@.service"
+USERNAME="live"           # User to update
+USERPASSWORDHASH=$(echo "$USERPASSWORD" | openssl passwd -6 -stdin) # Generate a password with a random salt
+SHADOW_FILE="./airootfs/etc/shadow"  # Path to your shadow file
 
 # Save original line
 ORIGINAL_LINE=$(grep '^ExecStart=' "$TARGET_FILE")
@@ -21,6 +27,9 @@ echo "✅ Original line backed up: $ORIGINAL_LINE"
 echo "🔧 Patching line for ISO 1..."
 sed -i "s|^ExecStart=.*|$ISO1_EXEC|" "$TARGET_FILE"
 
+echo "🔧 Patching shadow file for ISO 1..."
+sed -i "s|^$USERNAME:[^:]*:|$USERNAME:$USERPASSWORDHASH:|" "$SHADOW_FILE"
+
 # Build first ISO
 echo "▶️ Building first ISO..."
 mkarchiso -v -w ../work-iso1 -o ../out-iso1 "$CONFIG_DIR"
@@ -28,6 +37,7 @@ mkarchiso -v -w ../work-iso1 -o ../out-iso1 "$CONFIG_DIR"
 # Restore original line
 echo "♻️ Restoring original config..."
 sed -i "s|^ExecStart=.*|$ORIGINAL_LINE|" "$TARGET_FILE"
+sed -i "s|^$USERNAME:[^:]*:|$USERNAME:CHANGE_ME:|" "$SHADOW_FILE"
 
 echo "🔧 Moving ISO"
 # Get current build date in YYYY-MM-DD format
