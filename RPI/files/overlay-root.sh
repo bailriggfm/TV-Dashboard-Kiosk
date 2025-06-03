d4e644a380fd94881016ecf8a8239a43797b8c6a
Ava Glass <AvaAGlass@outlook.com>
2025-06-03 18:02:39 +0100

    Make sure we run the overlay config on the second boot

--- RPI/files/overlay-root.sh
+++ RPI/files/overlay-root.sh
@@ -5,6 +5,25 @@
 
 set -e
 
+if ! FWLOC=$(/usr/lib/raspberrypi-sys-mods/get_fw_loc); then
+  whiptail --msgbox "Could not determine firmware partition" 20 60
+  poweroff -f
+fi
+
+CMDLINE="$FWLOC/cmdline.txt"
+
+# Check if cmdline.txt exists
+if [ ! -f "$CMDLINE" ]; then
+    echo "Error: $CMDLINE not found."
+    exit 1
+fi
+
+# Check for the init override
+if grep -q 'init=/usr/lib/raspberrypi-sys-mods/firstboot' "$CMDLINE"; then
+    echo "Found init=/usr/lib/raspberrypi-sys-mods/firstboot in cmdline.txt. Exiting."
+    exit 0
+fi
+
 # If our flag doesnt exist set it and reboot
 if [ ! -f "/readonly-done" ]; then
     touch /readonly-done
