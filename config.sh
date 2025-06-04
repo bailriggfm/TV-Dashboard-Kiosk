97a7971251919d65ecb3334a607ab0361553c089
Ava Glass <AvaAGlass@outlook.com>
2025-06-04 01:26:17 +0100

    Bug fix

--- config.sh
+++ config.sh
@@ -9,25 +9,25 @@ export USERPASSWORD="liveuser"
 # Cage service configuration
 # Please note that some characters may need to be escaped to work with sed.
 # Such as &. If you want an & you must put \&
-export CAGEEXECSTART_LINE='ExecStart=/usr/bin/cage -s -- \
-    /usr/bin/firefox \
-    --kiosk \
-    --no-remote \
-    --start-fullscreen \
-    --no-proxy-server \
+export CAGEEXECSTART_LINE='ExecStart=/usr/bin/cage -s -- \\
+    /usr/bin/firefox \\
+    --kiosk \\
+    --no-remote \\
+    --start-fullscreen \\
+    --no-proxy-server \\
     "https://google.com"'
 
 # Option if using chromium instead of firefox
-#export CAGEEXECSTART_LINE='ExecStart=/usr/bin/cage -s -- \
-#    /usr/bin/chromium \
-#    --kiosk "https://google.com" \
-#    --noerrdialogs \
-#    --disable-infobars \
-#    --incognito \
-#    --start-fullscreen \
-#    --autoplay-policy=no-user-gesture-required \
-#    --disable-session-crashed-bubble \
-#    --no-proxy-server \
+#export CAGEEXECSTART_LINE='ExecStart=/usr/bin/cage -s -- \\
+#    /usr/bin/chromium \\
+#    --kiosk "https://google.com" \\
+#    --noerrdialogs \\
+#    --disable-infobars \\
+#    --incognito \\
+#    --start-fullscreen \\
+#    --autoplay-policy=no-user-gesture-required \\
+#    --disable-session-crashed-bubble \\
+#    --no-proxy-server \\
 #    --no-first-run'
 
 # x86_64 CONFIG
